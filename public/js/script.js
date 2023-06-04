e1820cbe483334b2e8301e4f822aa143c7cda2c1
0Hal0 <43924073+0Hal0@users.noreply.github.com>
2023-06-04 17:54:20 +0100

    Generalised post to airtable

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,6 @@
 SERVICE_TYPES_URI = "https://penninelancs.openplace.directory/o/ServiceDirectoryService/v2/taxonomies/?per_page=5000&vocabulary=esdServiceTypes";
 SERVICES_URI = "https://penninelancs.openplace.directory/o/ServiceDirectoryService/v2/services/local/?per_page=5000&service_type="
+ACCESS_TOKEN = "patBW3t4n3ebFIUBn.08d2769abf211012356fece8f65f8198ba07d74d4a459460ac8e017776564498"
 
 async function getServiceTypes(){
     serviceTypes = await fetch(SERVICE_TYPES_URI)
@@ -31,26 +32,30 @@ async function getServicesOfType(type){
 }
 
 async function postServicesToEndpoint(services){
+    baseID = "app2aHT8MrG4nYq4H";
+    servicesTableID = "tblHXa2jQ3OHjfczf";
     var orFormatServices = services.map(x => ({fields: {name: x.name, description: x.description}}))
-    //for (let i = 0; i < services.length; i++){
-    //    orFormatServices.append
-    //}
     if(orFormatServices.length > 10){
         orFormatServices = orFormatServices.slice(0, 10);
     }
     body = {records: orFormatServices}
 
+    await postToAirtable(baseID, servicesTableID, JSON.stringify(body));
+}
+
+async function postToAirtable(baseID, tableID, body){
+    airTableUrl = "https://api.airtable.com/v0";
     try{
-        await fetch("https://api.airtable.com/v0/app2aHT8MrG4nYq4H/tblHXa2jQ3OHjfczf",
+        await fetch(airTableUrl + "/" + baseID + "/" + tableID,
         {
             method: "POST",
             headers: new Headers({
-                "Authorization": "Bearer patBW3t4n3ebFIUBn.08d2769abf211012356fece8f65f8198ba07d74d4a459460ac8e017776564498",
+                "Authorization": "Bearer " + ACCESS_TOKEN,
                 "Content-Type": "application/json"
             }),
-            body: JSON.stringify(body)
+            body: body
         })
-    }catch{
-        console.log("Error")
+    }catch(error){
+        console.log(error)
     }
 }
