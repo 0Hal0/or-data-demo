0dbde38350c180dfa5186d072aeef307cec525f3
0Hal0 <43924073+0Hal0@users.noreply.github.com>
2023-06-04 17:45:13 +0100

    Added simple post for 10 or less services

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
 SERVICE_TYPES_URI = "https://penninelancs.openplace.directory/o/ServiceDirectoryService/v2/taxonomies/?per_page=5000&vocabulary=esdServiceTypes";
-SERVICES_URI = "https://penninelancs.openplace.directory/o/ServiceDirectoryService/v2/services/local/?service_type="
+SERVICES_URI = "https://penninelancs.openplace.directory/o/ServiceDirectoryService/v2/services/local/?per_page=5000&service_type="
 
 async function getServiceTypes(){
     serviceTypes = await fetch(SERVICE_TYPES_URI)
@@ -26,35 +26,29 @@ async function getServicesOfType(type){
     console.log(type);
     services = await fetch(SERVICES_URI + String(type));
     services = await services.json();
-    console.log(services);
+    services = services["content"]
     return services;
 }
 
 async function postServicesToEndpoint(services){
+    var orFormatServices = services.map(x => ({fields: {name: x.name, description: x.description}}))
+    //for (let i = 0; i < services.length; i++){
+    //    orFormatServices.append
+    //}
+    if(orFormatServices.length > 10){
+        orFormatServices = orFormatServices.slice(0, 10);
+    }
+    body = {records: orFormatServices}
+
     try{
-        await fetch("https://api.airtable.com/v0/app2aHT8MrG4nYq4H/tbl2giYwRZtciotzn",
+        await fetch("https://api.airtable.com/v0/app2aHT8MrG4nYq4H/tblHXa2jQ3OHjfczf",
         {
             method: "POST",
             headers: new Headers({
                 "Authorization": "Bearer patBW3t4n3ebFIUBn.08d2769abf211012356fece8f65f8198ba07d74d4a459460ac8e017776564498",
                 "Content-Type": "application/json"
             }),
-            body:JSON.stringify({
-                records: [
-                    {
-                      fields: {
-                        name: "test org name",
-                        description: "test org desc",
-                      }
-                    },
-                    {
-                      fields: {
-                        name: "test name 2",
-                        description: "test desc 2"
-                      }
-                    }
-                  ]
-            })
+            body: JSON.stringify(body)
         })
     }catch{
         console.log("Error")
