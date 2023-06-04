6098cd1ddb6fa9d2b48bfed78c2b9678a49383a9
0Hal0 <43924073+0Hal0@users.noreply.github.com>
2023-06-04 17:10:52 +0100

    Added test POST

--- public/js/script.js
+++ public/js/script.js
@@ -17,7 +17,9 @@ async function getServiceTypes(){
 async function update(){
     type = document.getElementById("service-type-input");
     type = document.getElementById(type.value);
-    getServicesOfType(type.innerHTML);
+    services = await getServicesOfType(type.innerHTML);
+
+    postServicesToEndpoint(services);
 }
 
 async function getServicesOfType(type){
@@ -25,8 +27,36 @@ async function getServicesOfType(type){
     services = await fetch(SERVICES_URI + String(type));
     services = await services.json();
     console.log(services);
+    return services;
 }
 
-async function postServicesToEndpoint(URI){
-
+async function postServicesToEndpoint(services){
+    try{
+        await fetch("https://api.airtable.com/v0/app2aHT8MrG4nYq4H/tbl2giYwRZtciotzn",
+        {
+            method: "POST",
+            headers: new Headers({
+                "Authorization": "Bearer patBW3t4n3ebFIUBn.08d2769abf211012356fece8f65f8198ba07d74d4a459460ac8e017776564498",
+                "Content-Type": "application/json"
+            }),
+            body:JSON.stringify({
+                records: [
+                    {
+                      fields: {
+                        name: "test org name",
+                        description: "test org desc",
+                      }
+                    },
+                    {
+                      fields: {
+                        name: "test name 2",
+                        description: "test desc 2"
+                      }
+                    }
+                  ]
+            })
+        })
+    }catch{
+        console.log("Error")
+    }
 }
