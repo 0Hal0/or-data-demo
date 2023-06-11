5f81cf009bd5329077efb933c448156da9db210c
0Hal0 <43924073+0Hal0@users.noreply.github.com>
2023-06-11 21:03:03 +0100

    Added service_at_locations post

--- controllers/or-data.controller.js
+++ controllers/or-data.controller.js
@@ -10,6 +10,9 @@ const servicesTableId = process.env.SERVICES_TABLE_ID;
 const orgTableId = process.env.ORGANIZATION_TABLE_ID;
 const contactsTableId = process.env.CONTACTS_TABLE_ID;
 const phonesTableId = process.env.PHONES_TABLE_ID;
+const serviceAtLocationsTableId = process.env.SERVICE_AT_LOCATIONS_TABLE_ID;
+const schedulesTableId = process.env.SCHEDULES_TABLE_ID;
+const locationsTableId = process.env.LOCATIONS_TABLE_ID;
 
 exports.updateTables = async (type) => {
     services = await getServicesOfType(type);
@@ -18,7 +21,15 @@ exports.updateTables = async (type) => {
 }
 
 exports.clearBase = async () => {
-    tables = [servicesTableId, orgTableId, contactsTableId, phonesTableId]
+    tables = [
+        servicesTableId,
+        orgTableId, 
+        contactsTableId, 
+        phonesTableId, 
+        serviceAtLocationsTableId,
+        schedulesTableId,
+        locationsTableId,
+    ]
     for(let i=0;i<tables.length;i++){
         //get records
         records = await fetch("https://api.airtable.com/v0/" + baseId + "/" + tables[i],
@@ -76,14 +87,95 @@ async function postServicesToEndpoint(orServices){
         orService = await getService(orServices[i]["id"]);
         //Post Service
         airTableService = await postService(orService);
+        airTableService = airTableService["records"][0];
         //Post organization
         airTableOrg = await postOrg(orService, airTableService);
+        airTableOrg = airTableOrg["records"][0];
+        airTableService["organization"] = airTableOrg;
         //Post contact
-        await postContactsAndPhones(orService, airTableService);
+        airTableContacts = await postContactsAndPhones(orService, airTableService);
+        if (airTableContacts != null){
+            airTableService["contacts"] = airTableContacts;
+        }else{ airTableService["contacts"] = []; }
+        
+        //Post service_at_location info
+        await postServiceAtLoction(orService, airTableService);
     }
 }
 
+async function postServiceAtLoction(orService, airTableService){
+    serviceAtLocations = orService["service_at_locations"]
+    for(let i = 0; i < serviceAtLocations.length; i++){
+        contacts = []
+        phones = []
+        for(let x = 0; x < airTableService["contacts"].length; x++){
+            contacts.push(airTableService["contacts"][x]["id"])
+            for(let y = 0; y < airTableService["contacts"][x]["phones"].length; y++){
+                phones.push(airTableService["contacts"][x]["phones"][y]["id"])
+            }
+        }
+        serviceAtLocation = {
+            fields: {
+                description: serviceAtLocations[i].description,
+                services: [airTableService["id"]],
+                phones: phones,
+                contacts : contacts,
+            }
+        }
+        body = {records : [serviceAtLocation]};
+        airTableServiceAtLocation = await postToAirtable(baseId, serviceAtLocationsTableId, JSON.stringify(body));
+        airTableServiceAtLocation = airTableServiceAtLocation["records"][0];
+
+            
+        //schedules
+        schedule = serviceAtLocations[i]["regular_schedule"][0];
+        schedule["services"] = [airTableService["id"]]
+        schedule["service_at_location"] = [airTableServiceAtLocation["id"]];
+        schedule["closes_at"] = convertTime(schedule["closes_at"]);
+        schedule["opens_at"] = convertTime(schedule["opens_at"]);
+        delete schedule["id"]
+        schedule = removeNullFields(schedule);
+        body = {records : [{fields : schedule}]};
+        airTableSchedule = await postToAirtable(baseId, schedulesTableId, JSON.stringify(body));
+        airTableSchedule = airTableSchedule["records"][0];
+        //locations
+        location = {
+            fields: {
+                name: serviceAtLocations[i]["location"]["name"],
+                services: [airTableService["id"]],
+                organization: [airTableService["organization"]["id"]],
+                contacts: [airTableService["contacts"][0]["id"]],
+                phones: [airTableService["contacts"][0]["phones"][0]["id"]],
+                schedules: [airTableSchedule["id"]],
+                service_at_location: [airTableServiceAtLocation["id"]],
+            }
+        }
+        body = {records : [location]};
+        airTableLocation = await postToAirtable(baseId, locationsTableId, JSON.stringify(body));
+        console.log(airTableLocation);
+        airTableLocation = airTableLocation["records"][0];
+        console.log(airTableLocation);
+
+    }
+
+}
+
+function removeNullFields(object){
+    for (var key in object){
+        if(object[key] == ""){
+            delete object[key];
+        }
+    }
+    return object;
+}
+
+function convertTime(time){
+    return "2020-09-05T" + time + ":00";
+}
+
+
 async function postContactsAndPhones(orService, airTableService){
+    var airTableContacts = [];
     contacts = orService["contacts"];
     for(let i = 0; i < contacts.length; i++){
         contact = {
@@ -91,28 +183,31 @@ async function postContactsAndPhones(orService, airTableService){
                 name: contacts[i].name,
                 title: contacts[i].title,
                 email: contacts[i].email,
-                services: [airTableService["records"][0]["id"]],
+                services: [airTableService["id"]],
             }
         }
         body = {records : [contact]};
         airTableContact = await postToAirtable(baseId, contactsTableId, JSON.stringify(body));
-
+        airTableContact = airTableContact["records"][0];
         phones = contacts[i].phones;
-        console.log(phones);
+        airTableContact["phones"] = [];
         for(let j=0; j<phones.length; j++){
             phone = {
                 fields: {
                     number: phones[j].number,
-                    contacts: [airTableContact["records"][0]["id"]],
-                    services: [airTableService["records"][0]["id"]],
+                    contacts: [airTableContact["id"]],
+                    services: [airTableService["id"]],
                 }
             }
             body = {records : [phone]};
-            phone = await postToAirtable(baseId, phonesTableId, JSON.stringify(body));
-            console.log(phone);
+            airTablePhone = await postToAirtable(baseId, phonesTableId, JSON.stringify(body));
+            airTablePhone = airTablePhone["records"][0];
+            airTableContact["phones"].push(airTablePhone);
         }
+        airTableContacts.push(airTableContact);
 
     }
+    return airTableContacts;
 }
 
 async function getService(serviceId){
@@ -129,7 +224,7 @@ async function postOrg(orService, airTableService){
             name: orService["organization"]["name"],
             description: orService["organization"]["description"],
             website: orService["organization"]["url"],
-            services: [airTableService["records"][0]["id"]]
+            services: [airTableService["id"]]
         }
     }
     body = {records : [org]};
