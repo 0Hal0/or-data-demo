13a84db523e73f55271c49f9d0e6a97dfe73423a
Hal <hal0dickinson@gmail.com>
2023-06-16 20:03:16 +0100

    Added services type filter

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,39 @@
 SERVICE_TYPES_URI = "https://penninelancs.openplace.directory/o/ServiceDirectoryService/v2/taxonomies/?per_page=5000&vocabulary=esdServiceTypes";
 APP_URL = "http://localhost:3000/"
 
+const SERVICE_TYPES = [
+    "728",
+    "727",
+    "1792",
+    "1806",
+    "190",
+    "328",
+    "298",
+    "19",
+    "643",
+    "272",
+    "343",
+    "831",
+    "1815",
+    "1805",
+    "1803",
+    "199",
+    "1804",
+    "1810",
+    "1814",
+    "112",
+    "189",
+    "242",
+    "1789",
+    "437",
+    "927",
+    "1284",
+    "1793",
+    "164",
+    "292",
+    "1114",
+    "1466",
+]
 async function getServiceTypes(){
     serviceTypes = await fetch(SERVICE_TYPES_URI)
     serviceTypes = await serviceTypes.json();
@@ -8,9 +41,11 @@ async function getServiceTypes(){
 
     var typeSuggestions = document.getElementById("service-type-suggestions");
     for (let i=0; i < serviceTypes.length; i++){
-        var option = new Option(serviceTypes[i]["id"], serviceTypes[i]["name"]);
-        option.id = serviceTypes[i]["name"];
-        typeSuggestions.appendChild(option);
+        if (SERVICE_TYPES.includes(serviceTypes[i]["id"])){
+            var option = new Option(serviceTypes[i]["id"], serviceTypes[i]["name"]);
+            option.id = serviceTypes[i]["name"];
+            typeSuggestions.appendChild(option);
+        }
     }
 }
 
